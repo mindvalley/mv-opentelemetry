3f261ed6c4abf086e9c4e4f4765b27557056792e
Maciej Szlosarczyk <maciej@sosek.net>
2021-10-01 13:15:14 +0300

    add JSON as dependency
    Signed-off-by: Maciej Szlosarczyk <maciej@mindvalley.com>

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule MvOpentelemetry.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:phoenix] ++ Mix.compilers(),
+      compilers: Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps(),
@@ -26,9 +26,9 @@ defmodule MvOpentelemetry.MixProject do
     ]
   end
 
-  defp extra_applications(:test), do: [:logger, :mv_opentelemetry_harness]
-  defp extra_applications(:dev), do: [:logger, :mv_opentelemetry_harness]
-  defp extra_applications(_), do: [:logger]
+  defp extra_applications(:test), do: [:logger, :runtime_tools, :mv_opentelemetry_harness]
+  defp extra_applications(:dev), do: [:logger, :runtime_tools, :mv_opentelemetry_harness]
+  defp extra_applications(_), do: [:logger, :runtime_tools]
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
@@ -40,6 +40,9 @@ defmodule MvOpentelemetry.MixProject do
       {:opentelemetry, "~> 0.6.0", override: true},
       {:opentelemetry_api, "~> 0.6.0", override: true},
       {:opentelemetry_telemetry, "~> 1.0.0-beta.2"},
+
+      # JSON
+      {:jason, "~> 1.0"},
 
       # Test and development harness
       {:mv_opentelemetry_harness, path: "./mv_opentelemetry_harness", only: [:dev, :test]},
