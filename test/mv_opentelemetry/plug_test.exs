a1d18486096e97819ad18e4a3061f55b973c7364
Maciej Szlosarczyk <maciej@mindvalley.com>
2022-01-11 14:18:07 +0200

    Bring Plug telemetry handler up to conventions
    * Set name to be "HTTP METHOD-NAME"
    * Rename http.request_path to http.target
    * Add flavor, transport and peer data into spans
    * Simplify the number of parameters required for setting up Plug
    tracer
    
    Signed-off-by: Maciej Szlosarczyk <maciej@mindvalley.com>

--- test/mv_opentelemetry/plug_test.exs
+++ test/mv_opentelemetry/plug_test.exs
@@ -3,42 +3,32 @@ defmodule MvOpentelemetry.PlugTest do
 
   test "handles successful requests in stories-specific context", %{conn: conn} do
     :otel_batch_processor.set_exporter(:otel_exporter_pid, self())
-
-    MvOpentelemetry.Plug.register_tracer(
-      tracer_id: :test_plug_tracer,
-      span_prefix: [:harness, :request],
-      name_prefix: [:mv_harness, :request]
-    )
+    MvOpentelemetry.Plug.register_tracer(span_prefix: [:harness, :request])
 
     conn
     |> put_req_header("user-agent", "Phoenix Test")
     |> put_req_header("referer", "http://localhost")
     |> get("/?query=1234&user_id=", %{})
 
-    assert_receive {:span, span(name: "mv_harness.request.get") = span_record}
+    assert_receive {:span, span(name: "HTTP GET") = span_record}
     {:attributes, _, _, _, attributes} = span(span_record, :attributes)
     keys = Enum.map(attributes, fn {k, _v} -> k end)
 
     assert {"http.status", 200} in attributes
     assert {"http.method", "GET"} in attributes
+    assert {"http.flavor", ""} in attributes
+    assert {"http.target", "/"} in attributes
     assert {"http.query_params.query", "1234"} in attributes
     assert {"http.query_params.user_id", ""} in attributes
     assert {"http.user_agent", "Phoenix Test"} in attributes
     assert {"http.referer", "http://localhost"} in attributes
+    assert {"net.transport", "IP.TCP"} in attributes
     assert "http.request_id" in keys
     assert "http.client_ip" in keys
+    assert "net.peer.ip" in keys
+    assert "net.peer.port" in keys
 
-    :ok = :telemetry.detach({:test_plug_tracer, MvOpentelemetry.Plug, :handle_start_event})
-    :ok = :telemetry.detach({:test_plug_tracer, MvOpentelemetry.Plug, :handle_stop_event})
-  end
-
-  test "defaults to [:phoenix, :endpoint] for span prefix", %{conn: conn} do
-    :otel_batch_processor.set_exporter(:otel_exporter_pid, self())
-    MvOpentelemetry.Plug.register_tracer(tracer_id: :test_new_plug_tracer)
-    get(conn, "/", %{})
-    assert_receive {:span, span(name: "phoenix.endpoint.get")}
-
-    :ok = :telemetry.detach({:test_new_plug_tracer, MvOpentelemetry.Plug, :handle_start_event})
-    :ok = :telemetry.detach({:test_new_plug_tracer, MvOpentelemetry.Plug, :handle_stop_event})
+    :ok = :telemetry.detach({[:harness, :request], MvOpentelemetry.Plug, :handle_start_event})
+    :ok = :telemetry.detach({[:harness, :request], MvOpentelemetry.Plug, :handle_stop_event})
   end
 end
