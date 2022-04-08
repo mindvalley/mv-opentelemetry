d141f2daaa3d20f8ef96984b2a5a5e91090c687c
Maciej Szlosarczyk <maciej@mindvalley.com>
2022-04-08 08:23:28 +0300

    Propagate context for dataloader tasks
    Signed-off-by: Maciej Szlosarczyk <maciej@mindvalley.com>

--- lib/mv_opentelemetry/dataloader.ex
+++ lib/mv_opentelemetry/dataloader.ex
@@ -13,6 +13,9 @@ defmodule MvOpentelemetry.Dataloader do
   def handle_event([:dataloader, :source, :run, :start], _measurements, meta, opts) do
     event_name = "dataloader.source.run"
 
+    parent_context = OpentelemetryProcessPropagator.fetch_parent_ctx(1, :"$callers")
+    attach_context(parent_context)
+
     OpentelemetryTelemetry.start_telemetry_span(__MODULE__, event_name, meta, %{})
     |> Span.set_attributes(opts[:default_attributes])
 
@@ -27,6 +30,9 @@ defmodule MvOpentelemetry.Dataloader do
   def handle_event([:dataloader, :source, :batch, :run, :start], _measurements, meta, opts) do
     event_name = "dataloader.source.batch.run"
 
+    parent_context = OpentelemetryProcessPropagator.fetch_parent_ctx(1, :"$callers")
+    attach_context(parent_context)
+
     OpentelemetryTelemetry.start_telemetry_span(__MODULE__, event_name, meta, %{})
     |> Span.set_attributes(opts[:default_attributes])
 
@@ -37,4 +43,7 @@ defmodule MvOpentelemetry.Dataloader do
     OpentelemetryTelemetry.end_telemetry_span(__MODULE__, meta)
     :ok
   end
+
+  defp attach_context(:undefined), do: :ok
+  defp attach_context(context), do: OpenTelemetry.Ctx.attach(context)
 end
