9ae6e6a8f0b500835eee9a759f9e56ab6431a8de
Maciej Szlosarczyk <maciej@mindvalley.com>
2023-09-08 15:48:02 +0300

    Cleanup Tesla test code (#56)
    Signed-off-by: Maciej Szlosarczyk maciej@mindvalley.com

--- test/mv_opentelemetry/tesla_test.exs
+++ test/mv_opentelemetry/tesla_test.exs
@@ -2,6 +2,7 @@ defmodule MvOpentelemetry.TeslaTest do
   use MvOpentelemetry.OpenTelemetryCase
 
   alias MvOpentelemetryHarnessWeb.Router.Helpers, as: Routes
+  require OpenTelemetry.Tracer, as: Tracer
 
   def setup_bypass(_) do
     bypass = Bypass.open()
@@ -34,16 +35,55 @@ defmodule MvOpentelemetry.TeslaTest do
 
     Tesla.get(client, "/")
 
-    assert_receive {:span, span(name: "HTTP get") = span_record}
+    assert_receive {:span, span(name: "HTTP GET") = span_record}
     {:attributes, _, _, _, attributes} = span(span_record, :attributes)
 
     assert :client == span(span_record, :kind)
 
     assert {:"http.status_code", 200} in attributes
-    assert {:"http.method", :get} in attributes
+    assert {:"http.method", "GET"} in attributes
     assert {:"http.target", "/"} in attributes
     assert {:"http.url", bypass_url} in attributes
     assert {"service.component", "test.harness"} in attributes
+
+    :ok = :telemetry.detach({:test_tesla_tracer, MvOpentelemetry.Tesla})
+  end
+
+  test "propagates telemetry context in Tesla from other processes", %{
+    tesla_client: client,
+    bypass: bypass,
+    bypass_url: bypass_url
+  } do
+    MvOpentelemetry.Tesla.register_tracer(
+      name: :test_tesla_tracer,
+      default_attributes: [{"service.component", "test.harness"}]
+    )
+
+    Bypass.expect(bypass, fn conn ->
+      Plug.Conn.resp(conn, 200, "")
+    end)
+
+    Tracer.with_span "root span", %{is_root: true} do
+      task =
+        Task.async(fn ->
+          Tesla.get(client, "/")
+        end)
+
+      Task.await(task)
+    end
+
+    assert_receive {:span, span(name: "root span") = span_record}
+    assert_receive {:span, span(name: "HTTP GET") = child_span_record}
+    {:attributes, _, _, _, attributes} = span(child_span_record, :attributes)
+
+    assert :client == span(child_span_record, :kind)
+    assert {:"http.status_code", 200} in attributes
+    assert {:"http.method", "GET"} in attributes
+    assert {:"http.target", "/"} in attributes
+    assert {:"http.url", bypass_url} in attributes
+    assert {"service.component", "test.harness"} in attributes
+
+    assert span(span_record, :span_id) == span(child_span_record, :parent_span_id)
 
     :ok = :telemetry.detach({:test_tesla_tracer, MvOpentelemetry.Tesla})
   end
@@ -58,7 +98,7 @@ defmodule MvOpentelemetry.TeslaTest do
 
     Tesla.get(client, url)
 
-    assert_receive {:span, span(name: "HTTP get") = span_record}
+    assert_receive {:span, span(name: "HTTP GET") = span_record}
     {:attributes, _, _, _, attributes} = span(span_record, :attributes)
 
     assert :client == span(span_record, :kind)
@@ -66,7 +106,7 @@ defmodule MvOpentelemetry.TeslaTest do
     keys = Enum.map(attributes, fn {k, _} -> k end)
 
     refute :"http.status_code" in keys
-    assert {:"http.method", :get} in attributes
+    assert {:"http.method", "GET"} in attributes
     assert {:"http.target", "/"} in attributes
     assert {:"http.url", url} in attributes
     assert {"service.component", "test.harness"} in attributes
