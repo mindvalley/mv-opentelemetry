01fb02a88801eec1a5c171e3c550996057f5e3bc
Maciej Szlosarczyk <maciej@mindvalley.com>
2024-03-01 13:05:17 +0200

    Update LiveView version in test harness
    Signed-off-by: Maciej Szlosarczyk maciej@mindvalley.com

--- mix.exs
+++ mix.exs
@@ -54,7 +54,7 @@ defmodule MvOpentelemetry.MixProject do
       {:mv_opentelemetry_harness, path: "./mv_opentelemetry_harness", only: [:dev, :test]},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:plug, "~> 1.0", optional: true},
-      {:phoenix_live_view, "~> 0.16", optional: true},
+      {:phoenix_live_view, "~> 0.18", optional: true},
       {:absinthe, "~> 1.0", optional: true},
       {:floki, ">= 0.30.0", only: :test},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
