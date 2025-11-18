b7a03033e2ff7f4fe13591c7be8468beb9fd37d2
Maciej Szlosarczyk <maciej@mindvalley.com>
2025-11-18 10:12:00 +0200

    Update deps harness (#82)

--- mix.exs
+++ mix.exs
@@ -59,6 +59,7 @@ defmodule MvOpentelemetry.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:plug, "~> 1.0", optional: true},
       {:phoenix_live_view, "~> 0.20 or ~> 1.0", optional: true},
+      {:lazy_html, ">= 0.1.0", only: :test, optional: true},
       {:absinthe, "~> 1.0", optional: true},
       {:floki, ">= 0.30.0", only: :test},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
