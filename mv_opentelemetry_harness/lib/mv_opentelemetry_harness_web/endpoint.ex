44d05100aacf1c2a818cea19395710fab645528b
Maciej Szlosarczyk <maciej@mindvalley.com>
2026-02-02 11:25:15 +0200

    Suppress compilation warning
    Signed-off-by: Maciej Szlosarczyk (maciej@mindvalley.com)

--- mv_opentelemetry_harness/lib/mv_opentelemetry_harness_web/endpoint.ex
+++ mv_opentelemetry_harness/lib/mv_opentelemetry_harness_web/endpoint.ex
@@ -1,6 +1,9 @@
 defmodule MvOpentelemetryHarnessWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :mv_opentelemetry_harness
 
+  # Suppress warning for dev-only dependency
+  @compile {:no_warn_undefined, Phoenix.LiveReloader}
+
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
   # Set :encryption_salt if you would also like to encrypt it.
